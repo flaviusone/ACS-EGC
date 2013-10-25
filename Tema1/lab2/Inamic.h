83efa55c8a499fa743aca26275623453bfa332b8
Tirnacop Flavius <flavius.tirnacop@cti.pub.ro>
2013-10-25 19:51:53 +0300

    [Tema_1] Reimplementat Inamic.h ca interfata

--- Tema1/lab2/Inamic.h
+++ Tema1/lab2/Inamic.h
@@ -1,11 +1,7 @@
 #pragma once
 #include "Framework\DrawingWindow.h"
-#include "Framework/Visual2D.h"
-#include "Framework/Transform2D.h"
-#include "Framework/Line2D.h"
-#include "Framework/Rectangle2D.h"
-#include "Framework/Circle2D.h"
-#include "Framework/Polygon2D.h"
+#include "Framework\Object2D.h"
+
 
 class Inamic
 {
@@ -13,20 +9,66 @@ public:
 	float directie;
 	float centrux,centruy;
 	//valorile pentru colision box
-	float bx1, bx2, by1, by2;	
+	float bxX, bxY;
 	int tip;
-	Rectangle2D *rectangle1;
-	Rectangle2D *rectangle2;
+	//partile din care este alcatuit inamicul
+	vector <Object2D*> parts;
 public:
 	Inamic(int tip, float dir, float startX, float startY);
-	Inamic Inamic1(float dir, float startX, float startY);
-//	Inamic Inamic2(float dir, float startX, float startY);
-//	Inamic Inamic3(float dir, float startX, float startY);
-	void addInamic2D();
-	void removeInamic2D();
-	void translate_to(float startX, float startY);
-	void translateInamic(float x, float y);
-	void calc_centru();
+	~Inamic(){}
+
+	// Adauga elementele inamicului la drawingwindow
+	void addInamic2D(){
+		for (int i = 0; i < parts.size(); i++)
+		{
+			DrawingWindow::addObject2D(parts[i]);
+		}
+	}
+
+	// Scoate elmentele inamicului din drawingwindow
+	void removeInamic2D(){
+		for (int i = 0; i < parts.size(); i++)
+		{
+			DrawingWindow::removeObject2D(parts[i]);
+		}
+	}
+
+	//TODO
+	//eventuala functie de rotire
+	void rotire();
+
+	// Translateaza inamicul la coord specificate
+	void translate_to(float X, float Y){
+		Transform2D::loadIdentityMatrix();
+		Transform2D::translateMatrix(X - centrux, Y - centruy);
+		for (int i = 0; i < parts.size(); i++){
+			Transform2D::applyTransform_o(parts[i]);
+		}
+	}
+
+	// Translateaza inamicul cu constanta primita
+	void translate_with(float X, float Y){
+		Transform2D::loadIdentityMatrix();
+		Transform2D::translateMatrix(X, Y);
+		for (int i = 0; i < parts.size(); i++){
+			Transform2D::applyTransform_o(parts[i]);
+		}
+	}
+	
+	// functie generica care calculeaza centrul obiectului folosind media aritmetica
+	void calc_centru(){
+		for (int i = 0; i < parts.size(); i++){
+			for (int j = 0; j < parts[i]->transf_points.size(); j++){
+				centrux += parts[i]->transf_points[j]->x;
+				centruy += parts[i]->transf_points[j]->y;
+			}
+			centrux = centrux / parts[i]->transf_points.size();
+			centruy = centruy / parts[i]->transf_points.size();
+		}
+		centrux = centrux / parts.size();
+		centruy = centruy / parts.size();
+	}
+	
 	 
 
 };
