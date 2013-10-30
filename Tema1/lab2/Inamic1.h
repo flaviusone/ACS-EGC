7d442dc5a47459989faf90f2cc3d94f001ea7875
Tirnacop Flavius <flavius.tirnacop@cti.pub.ro>
2013-10-31 01:42:18 +0200

    [Tema_1] Cleanup Inamic1.h

--- Tema1/lab2/Inamic1.h
+++ Tema1/lab2/Inamic1.h
@@ -4,28 +4,34 @@
 class Inamic1:public Inamic
 {
 public:
-	int aux;
 	Rectangle2D *rectangle1, *rectangle2;
 
 public:
 	Inamic1(float dir, float startX, float startY){
+
 		rectangle1 = new Rectangle2D(Point2D(0, 0), 20, 20, Color(0, 1, 0), false);
 		rectangle2 = new Rectangle2D(Point2D(10, 10), 20, 20, Color(0, 1, 0), false);
 		
 		//latime si lungime collision box	
-		bxX = 30;
-		bxY = 30;
+		bxX = 30; 	bxY = 30;
 
+		//Initializare hitbox
 		hitbox = new Rectangle2D(Point2D(0, 0), bxX, bxY, Color(0, 1, 0), false);
 
+		//Adauga la vector de obiecte
 		parts.push_back(rectangle1);
 		parts.push_back(rectangle2);
+
+		//Seteaza centrul
 		calc_centru();
+
+		//Muta la pozitia de start
 		translate_to(startX, startY);
+
+		//Seteaza directie tip valoare scor
 		directie = dir;
 		tip = 1;
 		value = 10;
-	
 	}
 	~Inamic1() {}
 
