0c7c79ad49dbf59c258f06c87c2b862167227b87
Tirnacop Flavius <flavius.tirnacop@cti.pub.ro>
2013-11-18 22:48:08 +0200

    [Tema_2] Adaugat clasa Inamic 2 (piramida)

---
+++ Tema2/lab2/Inamic2.h
@@ -0,0 +1,86 @@
+#pragma once
+#include "Framework\Point3D.h"
+#include "Framework\DrawingWindow.h"
+#include "Framework\Object3D.h"
+#include "Inamic.h"
+#define PI 3.14159265358979323846
+#define stanga 1
+#define dreapta -1
+
+class Inamic2 :public Inamic{
+
+public:
+	Inamic2(float startX, float startY, float startZ, float enemy_speed){
+		// set speed
+		speed = enemy_speed;
+
+		//varfurile de jos
+		vertices.push_back(new Point3D(0, 0, 0));
+		vertices.push_back(new Point3D(n, 0, 0));
+		vertices.push_back(new Point3D(n, 0, n));
+		vertices.push_back(new Point3D(0, 0, n));
+		//varful de sus
+		vertices.push_back(new Point3D(n / 2, 2*n, n / 2));
+
+		//cele 6 fete
+		vector <int> contour;
+		//fata jos
+		contour.clear();
+		contour.push_back(0);
+		contour.push_back(1);
+		contour.push_back(2);
+		contour.push_back(3);
+		faces.push_back(new Face(contour));
+
+		//fata fata
+		contour.clear();
+		contour.push_back(0);
+		contour.push_back(1);
+		contour.push_back(4);
+		faces.push_back(new Face(contour));
+
+		//fata spate
+		contour.clear();
+		contour.push_back(3);
+		contour.push_back(2);
+		contour.push_back(4);
+		faces.push_back(new Face(contour));
+
+		//fata stanga
+		contour.clear();
+		contour.push_back(0);
+		contour.push_back(3);
+		contour.push_back(4);
+		faces.push_back(new Face(contour));
+
+		//fata dreapta
+		contour.clear();
+		contour.push_back(1);
+		contour.push_back(2);
+		contour.push_back(4);
+		faces.push_back(new Face(contour));
+
+		// Create object
+		body = new Object3D(vertices, faces, Color(1, 0, 0), false);
+
+		// Set center position
+		calc_centru();
+
+		// Seteaza raza collision
+		raza = 70;
+
+		// Translate
+		tx += startX;
+		ty += startY;
+		tz += startZ;
+		Transform3D::translateMatrix(tx, ty, tz);
+		Transform3D::applyTransform(body);
+		// Add to visual
+		DrawingWindow::addObject3D(body);
+
+
+	}
+	~Inamic2(){};
+
+
+};
