0e4302c9f013d5e1632816b83e1ad6f578d402ab
Tirnacop Flavius <flavius.tirnacop@cti.pub.ro>
2013-12-20 01:39:46 +0200

    [Tema_3] Adaugat camere + miscare taste

--- Tema3/main.cpp
+++ Tema3/main.cpp
@@ -1,10 +1,10 @@
 ﻿//-------------------------------------------------------------------------------------------------
 // Descriere: fisier main
 //
-// Autor: student
+// Autor: Flavius Tirnacop 331CA
 // Data: today
 //-------------------------------------------------------------------------------------------------
-
+#pragma once
 //incarcator de meshe
 #include "lab_mesh_loader.hpp"
  
@@ -23,17 +23,28 @@
 //time
 #include <ctime>
 
+//Object
+#include"Object.hpp"
+
+#include <iostream>
+#include <windows.h>
+#include <string>
+#include <vector>
+
+
 
 class Laborator : public lab::glut::WindowListener{
 
 //variabile
 private:
 	glm::mat4 model_matrix, projection_matrix;											//matrici 4x4 pt modelare vizualizare proiectie
-	lab::Camera cam;
+	lab::Camera cam,cam1,cam2,cam3,cam4;
 	unsigned int gl_program_shader;														//id-ul de opengl al obiectului de tip program shader
 	unsigned int screen_width, screen_height;
-	unsigned int mesh_vbo, mesh_ibo, mesh_vao, mesh_num_indices;						//containere opengl pentru vertecsi, indecsi si stare
-
+	int active_camera = 0;
+	int up_pressed = 0, left_pressed = 0, right_pressed = 0, down_pressed = 0;
+	Object *player,*stadium;
+	std::vector <Object*> objects;
 //metode
 public:
 	
@@ -47,40 +58,115 @@ public:
 		//incarca un shader din fisiere si gaseste locatiile matricilor relativ la programul creat
 		gl_program_shader = lab::loadShader("shadere\\shader_vertex.glsl", "shadere\\shader_fragment.glsl");
 		
-		//incarca un mesh
-		lab::loadObj("resurse\\pod racer.obj",mesh_vao, mesh_vbo, mesh_ibo, mesh_num_indices);	
 
+		//Initializam pod_racer
+		player = new Object("resurse\\pod racer.obj");
+		player->scale(10, 10, 10);
+		objects.push_back(player);
+		//Initializam stadion
+		//stadium = new Object("resurse\\Stadion.obj");
+		//objects.push_back(stadium);
+		
 		//matrici de modelare si vizualizare
-		model_matrix = glm::mat4(1,0,0,0, 0,1,0,0, 0,0,1,0, 0,0,0,1);
-		cam.set(glm::vec3(0,0,40), glm::vec3(0,0,0), glm::vec3(0,1,0));
+		cam1.set(glm::vec3(0,0,40), glm::vec3(0,0,0), glm::vec3(0,1,0));
+		cam2.set(glm::vec3(0, 0, 40), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
+		cam3.set(glm::vec3(0, 0, 40), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
 
 		//desenare wireframe
 		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 		glLineWidth(10);
 		glPointSize(10);
+
 	}
 
 	//destructor .. e apelat cand e distrusa clasa
 	~Laborator(){
 		//distruge shader
 		glDeleteProgram(gl_program_shader);
-
-		//distruge mesh incarcat
-		glDeleteBuffers(1,&mesh_vbo);
-		glDeleteBuffers(1,&mesh_ibo);
-		glDeleteVertexArrays(1,&mesh_vao);
 	}
 
 	
+
+	void camera_1(Object* obj){
+		glViewport(0, 0, screen_width, screen_height);
+
+
+
+		//foloseste shaderul
+		glUseProgram(gl_program_shader);
+
+		//trimite variabile uniforme la shader
+		glUniformMatrix4fv(glGetUniformLocation(gl_program_shader, "model_matrix"), 1, false, glm::value_ptr(obj->model_matrix));
+		glUniformMatrix4fv(glGetUniformLocation(gl_program_shader, "view_matrix"), 1, false, glm::value_ptr(cam1.getViewMatrix()));
+		glUniformMatrix4fv(glGetUniformLocation(gl_program_shader, "projection_matrix"), 1, false, glm::value_ptr(projection_matrix));
+
+		//bind obiect
+		glBindVertexArray(obj->mesh_vao);
+		glDrawElements(GL_TRIANGLES, obj->mesh_num_indices, GL_UNSIGNED_INT, 0);
+
+		//deseneaza axe camera
+		glUniformMatrix4fv(glGetUniformLocation(gl_program_shader, "model_matrix"), 1, false, glm::value_ptr(glm::mat4()));	//identity
+		cam1.drawGeometry();
+	}
+	void camera_2(Object* obj){
+		//glViewport(screen_width / 2, 0, screen_width / 2, screen_height);
+		glViewport(0, 0, screen_width, screen_height);
+
+		//foloseste shaderul
+		glUseProgram(gl_program_shader);
+
+		//trimite variabile uniforme la shader
+		glUniformMatrix4fv(glGetUniformLocation(gl_program_shader, "model_matrix"), 1, false, glm::value_ptr(obj->model_matrix));
+		glm::mat4 view_matrix = glm::lookAt(glm::vec3(50, 50, 50), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
+		glUniformMatrix4fv(glGetUniformLocation(gl_program_shader, "view_matrix"), 1, false, glm::value_ptr(view_matrix));
+		glUniformMatrix4fv(glGetUniformLocation(gl_program_shader, "projection_matrix"), 1, false, glm::value_ptr(projection_matrix));
+
+		//bind obiect
+		glBindVertexArray(obj->mesh_vao);
+		glDrawElements(GL_TRIANGLES, obj->mesh_num_indices, GL_UNSIGNED_INT, 0);
+
+		//deseneaza axe camera
+		glUniformMatrix4fv(glGetUniformLocation(gl_program_shader, "model_matrix"), 1, false, glm::value_ptr(glm::mat4()));	//identity
+		cam1.drawGeometry();
+	}
+	void camera_3(Object* obj){
+		//glViewport(screen_width / 2, 0, screen_width / 2, screen_height);
+		glViewport(0, 0, screen_width, screen_height);
+
+		//foloseste shaderul
+		glUseProgram(gl_program_shader);
+
+		//trimite variabile uniforme la shader
+		glUniformMatrix4fv(glGetUniformLocation(gl_program_shader, "model_matrix"), 1, false, glm::value_ptr(obj->model_matrix));
+		glm::mat4 view_matrix = glm::lookAt(glm::vec3(0, 300, 0), glm::vec3(0, 0, 0), glm::vec3(-1, 0, 0));
+		glUniformMatrix4fv(glGetUniformLocation(gl_program_shader, "view_matrix"), 1, false, glm::value_ptr(view_matrix));
+		glUniformMatrix4fv(glGetUniformLocation(gl_program_shader, "projection_matrix"), 1, false, glm::value_ptr(projection_matrix));
+
+		//bind obiect
+		glBindVertexArray(obj->mesh_vao);
+		glDrawElements(GL_TRIANGLES, obj->mesh_num_indices, GL_UNSIGNED_INT, 0);
+
+		//deseneaza axe camera
+		glUniformMatrix4fv(glGetUniformLocation(gl_program_shader, "model_matrix"), 1, false, glm::value_ptr(glm::mat4()));	//identity
+		cam1.drawGeometry();
+	}
 	//--------------------------------------------------------------------------------------------
 	//functii de cadru ---------------------------------------------------------------------------
 
 	//functie chemata inainte de a incepe cadrul de desenare, o folosim ca sa updatam situatia scenei ( modelam/simulam scena)
 	void notifyBeginFrame(){
-		//rotatie
-		static float angle=0;
-		angle = 0.1f;
-		model_matrix = glm::rotate(model_matrix,angle,glm::vec3(0,1,0));
+		if (up_pressed){
+			player->translate(0.1, 0, 0);
+		}
+		if (down_pressed){
+			player->translate(-0.1, 0, 0);
+		}
+		if (right_pressed){
+			player->translate(0, 0, 0.1);
+		}
+		if (left_pressed){
+			player->translate(0, 0, -0.1);
+		}
 	}
 	//functia de afisare (lucram cu banda grafica)
 	void notifyDisplayFrame(){
@@ -88,45 +174,31 @@ public:
 		//pe tot ecranul
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		
-		//cadru normal
+		switch (active_camera)
 		{
-			glViewport(0,0, screen_width/2, screen_height);
-			
-			//foloseste shaderul
-			glUseProgram(gl_program_shader);
-				
-			//trimite variabile uniforme la shader
-			glUniformMatrix4fv(glGetUniformLocation(gl_program_shader,"model_matrix"),1,false,glm::value_ptr(model_matrix));
-			glUniformMatrix4fv(glGetUniformLocation(gl_program_shader,"view_matrix"),1,false,glm::value_ptr(cam.getViewMatrix()));
-			glUniformMatrix4fv(glGetUniformLocation(gl_program_shader,"projection_matrix"),1,false,glm::value_ptr(projection_matrix));
-		
-			//bind obiect
-			glBindVertexArray(mesh_vao);
-			glDrawElements(GL_TRIANGLES, mesh_num_indices, GL_UNSIGNED_INT, 0);
+		case 1: /* FPS camera*/
+			for (int i = 0; i < objects.size();i++){
+				camera_1(objects[i]);
+			}
+			break;
+		case 2: /* TPS camera*/
+			for (int i = 0; i < objects.size(); i++){
+				camera_2(objects[i]);
+			}
+			break;
+		case 3: /* FPS-UP camera*/	
+			for (int i = 0; i < objects.size(); i++){
+				camera_3(objects[i]);
+			}
+			break;
+		default: /* default camera 4*/
+			for (int i = 0; i < objects.size(); i++){
+				camera_1(objects[i]);
+			}
+			break;
 		}
-		
-		//cadru vazut din alt punct de vedere
-		{
-			glViewport(screen_width/2,0, screen_width/2, screen_height);
-			
-			//foloseste shaderul
-			glUseProgram(gl_program_shader);
-				
-			//trimite variabile uniforme la shader
-			glUniformMatrix4fv(glGetUniformLocation(gl_program_shader,"model_matrix"),1,false,glm::value_ptr(model_matrix));
-			glm::mat4 view_matrix = glm::lookAt(glm::vec3(50,50,50), glm::vec3(0,0,0), glm::vec3(0,1,0));
-			glUniformMatrix4fv(glGetUniformLocation(gl_program_shader,"view_matrix"),1,false,glm::value_ptr(view_matrix));
-			glUniformMatrix4fv(glGetUniformLocation(gl_program_shader,"projection_matrix"),1,false,glm::value_ptr(projection_matrix));
-		
-			//bind obiect
-			glBindVertexArray(mesh_vao);
-			glDrawElements(GL_TRIANGLES, mesh_num_indices, GL_UNSIGNED_INT, 0);
 
-			//deseneaza axe camera
-			glUniformMatrix4fv(glGetUniformLocation(gl_program_shader,"model_matrix"),1,false,glm::value_ptr(glm::mat4()));	//identity
-			cam.drawGeometry();
-		}
-		
+	
 	}
 	//functie chemata dupa ce am terminat cadrul de desenare (poate fi folosita pt modelare/simulare)
 	void notifyEndFrame(){}
@@ -157,25 +229,30 @@ public:
 			wire=!wire;
 			glPolygonMode(GL_FRONT_AND_BACK, (wire?GL_LINE:GL_FILL));
 		}
-		if(key_pressed == 'w') { cam.translateForward(1.0f); }
-		if(key_pressed == 'a') { cam.translateRight(-1.0f); }
-		if(key_pressed == 's') { cam.translateForward(-1.0f); }
-		if(key_pressed == 'd') { cam.translateRight(1.0f); }
-		if(key_pressed == 'r') { cam.translateUpword(1.0f); }
-		if(key_pressed == 'f') { cam.translateUpword(-1.0f); }
-		if(key_pressed == 'q') { cam.rotateFPSoY(1.0f); }
-		if(key_pressed == 'e') { cam.rotateFPSoY(-1.0f); }
-		if(key_pressed == 'z') { cam.rotateFPSoZ(-1.0f); }
-		if(key_pressed == 'c') { cam.rotateFPSoZ(1.0f); }
-		if(key_pressed == 't') { cam.rotateFPSoX(1.0f); }
-		if(key_pressed == 'g') { cam.rotateFPSoX(-1.0f); }
-		if(key_pressed == 'o') { cam.set(glm::vec3(0,0,40), glm::vec3(0,0,0), glm::vec3(0,1,0)); }
-		if(key_pressed == '1') { cam.rotateTPSoX(1.0f,40.0f); }
-		if(key_pressed == '2') { cam.rotateTPSoX(-1.0f,40.0f); }
-		if(key_pressed == '3') { cam.rotateTPSoY(1.0f,40.0f); }
-		if(key_pressed == '4') { cam.rotateTPSoY(-1.0f,40.0f); }
-		if(key_pressed == '5') { cam.rotateTPSoZ(1.0f,40.0f); }
-		if(key_pressed == '6') { cam.rotateTPSoZ(-1.0f,40.0f); }
+		if(key_pressed == 'w') { cam1.translateForward(1.0f); }
+		if(key_pressed == 'a') { cam1.translateRight(-1.0f); }
+		if(key_pressed == 's') { cam1.translateForward(-1.0f); }
+		if(key_pressed == 'd') { cam1.translateRight(1.0f); }
+		if(key_pressed == 'r') { cam1.translateUpword(1.0f); }
+		if(key_pressed == 'f') { cam1.translateUpword(-1.0f); }
+		if(key_pressed == 'q') { cam1.rotateFPSoY(1.0f); }
+		if(key_pressed == 'e') { cam1.rotateFPSoY(-1.0f); }
+		if(key_pressed == 'z') { cam1.rotateFPSoZ(-1.0f); }
+		if(key_pressed == 'c') { cam1.rotateFPSoZ(1.0f); }
+		if(key_pressed == 't') { cam1.rotateFPSoX(1.0f); }
+		if(key_pressed == 'g') { cam1.rotateFPSoX(-1.0f); }
+		if(key_pressed == 'o') { cam1.set(glm::vec3(0,0,40), glm::vec3(0,0,0), glm::vec3(0,1,0)); }
+		if(key_pressed == '1') { cam1.rotateTPSoX(1.0f,40.0f); }
+		if(key_pressed == '2') { cam1.rotateTPSoX(-1.0f,40.0f); }
+		if(key_pressed == '3') { cam1.rotateTPSoY(1.0f,40.0f); }
+		if(key_pressed == '4') { cam1.rotateTPSoY(-1.0f,40.0f); }
+		if(key_pressed == '5') { cam1.rotateTPSoZ(1.0f,40.0f); }
+		if(key_pressed == '6') { cam1.rotateTPSoZ(-1.0f,40.0f); }
+		if (key_pressed == '7') { active_camera = 1; }
+		if (key_pressed == '8') { active_camera = 2; }
+		if (key_pressed == '9') { active_camera = 3; }
+		if (key_pressed == '0') { active_camera = 4; }
+		
 	}
 	//tasta ridicata
 	void notifyKeyReleased(unsigned char key_released, int mouse_x, int mouse_y){	}
@@ -183,9 +260,18 @@ public:
 	void notifySpecialKeyPressed(int key_pressed, int mouse_x, int mouse_y){
 		if(key_pressed == GLUT_KEY_F1) lab::glut::enterFullscreen();
 		if(key_pressed == GLUT_KEY_F2) lab::glut::exitFullscreen();
+		if (key_pressed == GLUT_KEY_UP) { up_pressed = 1; }
+		if (key_pressed == GLUT_KEY_DOWN) { down_pressed = 1; }
+		if (key_pressed == GLUT_KEY_LEFT) { left_pressed = 1; }
+		if (key_pressed == GLUT_KEY_RIGHT) { right_pressed = 1; }
 	}
 	//tasta speciala ridicata
-	void notifySpecialKeyReleased(int key_released, int mouse_x, int mouse_y){}
+	void notifySpecialKeyReleased(int key_released, int mouse_x, int mouse_y){
+		if (key_released == GLUT_KEY_UP) { up_pressed = 0; }
+		if (key_released == GLUT_KEY_DOWN) { down_pressed = 0; }
+		if (key_released == GLUT_KEY_LEFT) { left_pressed = 0; }
+		if (key_released == GLUT_KEY_RIGHT) { right_pressed = 0; }
+	}
 	//drag cu mouse-ul
 	void notifyMouseDrag(int mouse_x, int mouse_y){ }
 	//am miscat mouseul (fara sa apas vreun buton)
